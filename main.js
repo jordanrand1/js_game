1cea7ff4dd082a796b5860bb0b535005b4fbc15e
Jordan Rand <jorrand@gmail.com>
2018-08-23 17:31:11 -0600

    add game

---
+++ main.js
@@ -0,0 +1,47 @@
+// Variables
+
+var boxes = $('.box')
+var $empty = $('#empty')
+var $shuffle = $('#shuffle')
+
+// Functions
+
+function move(e){
+  var emptyOrder = $($empty).css('order')
+  console.log($(e.target).css('order'))
+  isNearEmpty(e.target, emptyOrder)
+}
+
+function isNearEmpty(clickedOn, emptyOrder){
+  distance = $(clickedOn).css('order') - emptyOrder
+  if (distance === -1 || distance === -3 || distance === 1 || distance === 3){
+    moveBox(clickedOn, emptyOrder)
+  } else {
+    console.log('I can not be moved')
+  }
+}
+
+function moveBox(clickedOn, emptyOrder){
+  tempOrder = $(clickedOn).css('order')
+  $(clickedOn).css('order', emptyOrder)
+  $($empty).css('order', tempOrder)
+}
+
+function shuffle(e){
+  numbers = [1,2,3,4,5,6,7,8]
+  for (i = 0; i < boxes.length; i++){
+    randomIndex = Math.floor(Math.random() * numbers.length)
+    item = numbers[randomIndex]
+    numbers.splice(randomIndex, 1)
+    $(boxes[i]).css('order', item)
+    $($empty).css
+  }
+}
+
+// Listeners
+
+for (i = 0; i < boxes.length; i++){
+  $(boxes[i]).on('click', move)
+}
+
+$($shuffle).on('click', shuffle)
